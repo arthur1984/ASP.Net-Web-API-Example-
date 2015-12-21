f2a9ea7c0eab5e9bd2562a3d8663adf456cb1c7d
Arthur Nersesyan <arthur.nersesya@gmail.com>
2015-12-21 21:56:23 +0400

    Adding files

---
+++ RestAPI/Servises/CompanyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestAPI.Models;
+
+namespace RestAPI.Servises
+{
+    public class CompanyService
+    {
+        public static bool Create(Company company)
+        {
+            bool result = false;
+            try
+            {
+                //Call create company method
+                result = true;
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+    }
+}
